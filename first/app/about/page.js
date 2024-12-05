2d4ae7692b3d9352970a34c3fc568a7e9e08ce44
AnkushSatpute2005 <ankushsatpute2005@gmail.com>
2024-12-05 21:58:14 +0530

    Commited on 5 dec 2024 9:50 pm

---
+++ first/app/about/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      I am about
+      <h1 className="text-2xl"> Features of next.js</h1>
+      <ul>
+        <li>Full stak solution</li>
+        <li>File based Routing</li>
+        <li>Additional features like router from next/navigation</li>
+        <li>optinize rendering</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
